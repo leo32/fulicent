16a48257f811f9e18eedbef46022b1e268150e87
leo32 <24127063@qq.com>
2019-05-04 10:56:50 +0800

    change the release script Signed-off-by: leo32 <24127063@qq.com>

--- Script/release.bat
+++ Script/release.bat
@@ -1,8 +1,12 @@
 @echo off
-set fulicentPath=D:\GIT\My\fulicent
+set fulicentPath=G:\work\fulicent
 cd "%fulicentPath%\fulicentUI"
-cml web build
-pause
+rem cml web build
+
 xcopy "%fulicentPath%\fulicentUI\dist\web\*.*" "%fulicentPath%\fulicentApi\src\main\resources" /s/h/c/y
 cd "%fulicentPath%\fulicentApi"
-mvn package
+rem mvn package
+xcopy "target\fulicent-0.0.1-SNAPSHOT.jar" "%fulicentPath%\release\"  /s/h/c/y
+xcopy "%fulicentPath%\Script\Dockerfile" "%fulicentPath%\release\"  /s/h/c/y
+cd "%fulicentPath%\release\"
+docker build -t fulicent .

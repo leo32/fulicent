54ae3fe8fbf341cc9b1db965c088926d4dc39aa7
leo32 <24127063@qq.com>
2019-05-05 15:20:23 +0800

    change the bat
    Signed-off-by: leo32 <24127063@qq.com>

--- Script/release.bat
+++ Script/release.bat
@@ -1,11 +1,10 @@
 @echo off
 set fulicentPath=G:\work\fulicent
 cd "%fulicentPath%\fulicentUI"
-rem cml web build
-
+call cml web build
 xcopy "%fulicentPath%\fulicentUI\dist\web\*.*" "%fulicentPath%\fulicentApi\src\main\resources" /s/h/c/y
 cd "%fulicentPath%\fulicentApi"
-rem mvn package
+call mvn package
 xcopy "target\fulicent-0.0.1-SNAPSHOT.jar" "%fulicentPath%\release\"  /s/h/c/y
 xcopy "%fulicentPath%\Script\Dockerfile" "%fulicentPath%\release\"  /s/h/c/y
 cd "%fulicentPath%\release\"

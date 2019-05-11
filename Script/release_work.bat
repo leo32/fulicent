34811cfa28d50cab7eebb13e1a5f8dcca3268f0d
MSDOMAIN1\wchen3 <wenguang.chen@morningstar.com>
2019-05-11 17:23:18 +0800

    add work space

---
+++ Script/release_work.bat
@@ -0,0 +1,16 @@
+@echo off
+set fulicentPath=D:\GIT\My\fulicent
+cd "%fulicentPath%\fulicentUI"
+call cml web build
+cd "%fulicentPath%\Script\"
+sleep 120
+rename "%fulicentPath%\fulicentUI\dist\web\fulicentUI.html index.html
+xcopy "%fulicentPath%\fulicentUI\dist\web\*.*" "%fulicentPath%\fulicentApi\src\main\resources" /s/h/c/y
+cd "%fulicentPath%\fulicentApi"
+call mvn package
+cd "%fulicentPath%\Script\"
+sleep 120
+xcopy "%fulicentPath%\fulicentApi\target\fulicent-0.0.1-SNAPSHOT.jar" "%fulicentPath%\release\"  /s/h/c/y
+xcopy "%fulicentPath%\Script\Dockerfile" "%fulicentPath%\release\"  /s/h/c/y
+cd "%fulicentPath%\release\"
+docker build -t fulicent .

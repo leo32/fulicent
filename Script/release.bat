1ab82ecee1ea125eef339ad36a038407c47da980
leo32 <24127063@qq.com>
2019-05-12 10:41:48 +0800

    fix FulicentUI. Signed-off-by: leo32 <24127063@qq.com>

--- Script/release.bat
+++ Script/release.bat
@@ -2,11 +2,15 @@
 set fulicentPath=G:\work\fulicent
 cd "%fulicentPath%\fulicentUI"
 call cml web build
+cd "%fulicentPath%\Script\"
+sleep 60
 rename "%fulicentPath%\fulicentUI\dist\web\fulicentUI.html index.html
-xcopy "%fulicentPath%\fulicentUI\dist\web\*.*" "%fulicentPath%\fulicentApi\src\main\resources" /s/h/c/y
+xcopy "%fulicentPath%\fulicentUI\dist\web\*.*" "%fulicentPath%\fulicentApi\src\main\resources\static" /s/h/c/y
 cd "%fulicentPath%\fulicentApi"
 call mvn package
-xcopy "target\fulicent-0.0.1-SNAPSHOT.jar" "%fulicentPath%\release\"  /s/h/c/y
+cd "%fulicentPath%\Script\"
+sleep 60
+xcopy "%fulicentPath%\fulicentApi\target\fulicent-0.0.1-SNAPSHOT.jar" "%fulicentPath%\release\"  /s/h/c/y
 xcopy "%fulicentPath%\Script\Dockerfile" "%fulicentPath%\release\"  /s/h/c/y
 cd "%fulicentPath%\release\"
-docker build -t fulicent .
+rem docker build -t fulicent .
--- fulicentUI/src/store/state.js
+++ fulicentUI/src/store/state.js
@@ -1,5 +1,11 @@
 
-const state = {
+const state = {  tabs: [
+    {label: '首页'},
+    {label: "超级人气榜"},
+    {label: '特卖精选'},
+    {label: '品牌优惠券'},
+    {label: '我的足迹'}
+  ]
 }
 
 export default state
